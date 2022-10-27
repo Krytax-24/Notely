8cf92b777c3de098a6b5a8f875bbec2ceb92da79
Krishna Rajkumar Dembla <81406458+Krytax-24@users.noreply.github.com>
2022-10-27 12:48:31 +0530

    Added new sections and Navbar properties

--- script/script.js
+++ script/script.js
@@ -1,160 +1,17 @@
-searchForm = document.querySelector('.search-form');
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
 
-document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
-}
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-let loginForm = document.querySelector('.login-form-container');
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
-}
-
-document.querySelector('#close-login-btn').onclick = () => {
-    loginForm.classList.remove('active');
-}
-
-window.onscroll = () => {
-
-    searchForm.classList.remove('active');
-
-    if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
     } else {
-        document.querySelector('.header .header-2').classList.remove('active');
+        navbar.classList.remove("sticky");
     }
-
 }
-
-window.onload = () => {
-
-    if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
-    } else {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
-
-    fadeOut();
-
-}
-
-function loader() {
-    document.querySelector('.loader-container').classList.add('active');
-}
-
-function fadeOut() {
-    setTimeout(loader, 4000);
-}
-
-var swiper = new Swiper(".books-slider", {
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".featured-slider", {
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        450: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
-    },
-});
-
-var swiper = new Swiper(".arrivals-slider", {
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".reviews-slider", {
-    spaceBetween: 10,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 10,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
