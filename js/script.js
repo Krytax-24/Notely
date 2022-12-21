0afa76af96d833ee5ffd8743e867589e3238d279
Krishna Rajkumar Dembla <81406458+Krytax-24@users.noreply.github.com>
2022-12-21 11:12:58 +0530

    card js update

--- js/script.js
+++ js/script.js
@@ -47,12 +47,12 @@ function fadeOut() {
 }
 
 var swiper = new Swiper(".books-slider", {
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
+    // loop: true,
+    // centeredSlides: true,
+    // autoplay: {
+    //     delay: 9500,
+    //     disableOnInteraction: false,
+    // },
     breakpoints: {
         0: {
             slidesPerView: 1,
@@ -68,16 +68,16 @@ var swiper = new Swiper(".books-slider", {
 
 var swiper = new Swiper(".featured-slider", {
     spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    // loop: true,
+    // centeredSlides: true,
+    // autoplay: {
+    //     delay: 9500,
+    //     disableOnInteraction: false,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
     breakpoints: {
         0: {
             slidesPerView: 1,
@@ -90,71 +90,6 @@ var swiper = new Swiper(".featured-slider", {
         },
         1024: {
             slidesPerView: 4,
-        },
-    },
-});
-
-var swiper = new Swiper(".arrivals-slider", {
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".reviews-slider", {
-    spaceBetween: 10,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 10,
-    grabCursor: true,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 9500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
         },
     },
 });
